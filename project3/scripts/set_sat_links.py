37b9508157236ef40dd99bc78536661af0ece956
eschudel <eschudel@ethz.ch>
2019-04-30 11:02:25 +0200

    statically maximize ISL distance

--- project3/scripts/set_sat_links.py
+++ project3/scripts/set_sat_links.py
@@ -10,19 +10,17 @@ satPositions = util.read_sat_positions("../input_data/sat_positions.txt")
 with open("../output_data/sat_links.txt", 'w') as file:
 	# 40 satellites per orbit, 40 orbits
 	for i in range(40):
-		#in each orbit, connect neighboring satellites, 0 -> 1, 1 -> 2
+		#in each orbit, connect satellites on same orbit, maximize distance, 0 -> 4, 1 -> 5
 		for j in range(40):
 			sat1 = i*40+j
-			sat2 = i*40+j+1
-			if (j==39):
-				sat2 = i*40
+			sat2 = (j+4)%(40)+i*40
 			file.write("%s\n" %(",".join([str(sat1), str(sat2), str(util.compute_isl_length(sat1, sat2, satPositions))])))
 
-	# connect to orbit k -1
+	# connect to satelites in orbit k -3
 	for k in range(1,40):
 		for l in range(40):
 			sat1 = k*40 + l
-			sat2 = (k-1)*40 + l
+			sat2 = ((k-3)%40)*40 + l
 			file.write("%s\n" %(",".join([str(sat1), str(sat2), str(util.compute_isl_length(sat1, sat2, satPositions))])))
 
 
